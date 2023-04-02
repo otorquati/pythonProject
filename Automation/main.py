8a8885ec8d65acfd2364caaa68f0c48a2587f64e
Osvaldo Torquati <62728159+otorquati@users.noreply.github.com>
2023-04-02 01:31:36 -0300

    Automation
    Automation with python - final version

--- Automation/main.py
+++ Automation/main.py
@@ -1,20 +1,53 @@
 import openpyxl
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
+# Abre o arquivo e cola em uma variável workbook
+inv_file = openpyxl.load_workbook("./Automation/inventory.xlsx")
 
-product_list = inv_file["sheet1"]
+#Coloca uma planilha específica em variável
+product_list = inv_file['Sheet1']
 
 # Exercício 1: Listar cada comapnhia e seus recpectivos produtos
+# Cria um dicionário vazio de produtos por fornecedor
 products_per_supplier = {}
-print(product_list.max_row)
+total_value_per_supplier = {}
+products_under_10inv = {}
 
+# Cria um loop para a faixa "range" de 2 a 75 (max_row)
 for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row,4)
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_number = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+    # Calcula número de produtos por fornecedor
     if supplier_name in products_per_supplier:
         current_num_products = products_per_supplier[supplier_name]
         products_per_supplier[supplier_name] = current_num_products + 1
     else:
-        print("Adicionando Fornecedor")
+        # print("Adicionando Fornecedor")
         products_per_supplier[supplier_name] = 1
 
+# Exercício 2: Calcular quantidade de produtos por fornecedor
+    if supplier_name in total_value_per_supplier:
+       current_total_value = total_value_per_supplier.get(supplier_name) 
+       total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+# Exercício 3: produtos com qutd menro que 10
+    if inventory < 10:
+        products_under_10inv[product_number]=int(inventory)
+
+# Exercício 4: Escrever resultados na planilha
+    # Adicionando valores 
+    inventory_price.value = price * inventory
+
+
+# Resultado Exercício 1
 print(products_per_supplier)
+# Resultado Exercício 2
+print(total_value_per_supplier)
+# Resultado Exercício 3
+print(products_under_10inv)
+# Resultado Exercício 4
+inv_file.save("./Automation/inventory_updated.xlsx")
